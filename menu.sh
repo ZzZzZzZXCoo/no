998e16c8c38c3e279801b968cabf52b3329ab5de
ZzZzZzZXCoo <77143312+ZzZzZzZXCoo@users.noreply.github.com>
2021-06-02 20:26:24 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -94,6 +94,7 @@ echo -e "$red  55.  $white Reboot VPS"
 echo -e "$red  56.  $white Speedtest VPS"
 echo -e "$red  57.  $white Information Display System"
 echo -e "$red  58.  $white Info Script Auto Install"
+echo -e "$red  59.  $white Install SSH Websocket"
 echo -e "$red═══════════════════════════════════════════"
 echo -e "$red   0.  $white Exit From Putty / JuiceSSH / Termux"
 echo -e "$gren═══════════════════════════════════════════"
@@ -274,6 +275,9 @@ info
 ;;
 58)
 about
+;;
+59)
+edu
 ;;
 x)
 exit
